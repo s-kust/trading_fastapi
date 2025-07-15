277c65638914dccd0eab1ee0412022540dbfc27c
SergioKushchenko <serhii.kushchenko@appvales.com>
2025-07-15 14:40:31 +0200

    Added logging

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
+import logging
+from logging.config import dictConfig
+
 import yfinance as yf
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
 from constants import S3_BUCKET, S3_FOLDER_DAILY_DATA
+from utils.log_config import log_config
 
 # from utils.s3 import read_daily_ohlc_from_s3
 
+dictConfig(log_config)
+logger = logging.getLogger("app")
 load_dotenv()
+
 app = FastAPI()
+
 @app.get("/")
 async def root() -> dict:
     # df = read_daily_ohlc_from_s3(ticker="A")
-    df = yf.Ticker("MSFT").history(period='max', interval='1d')
+    ticker="MSFT"
+    # df = yf.Ticker(ticker=ticker).history(period='max', interval='1d')
+    logger.debug(f"This is a debug message with {ticker=}")
     return {"message": "Hello World 11", 
         "S3_BUCKET": S3_BUCKET,
         "S3_FOLDER_DAILY_DATA": S3_FOLDER_DAILY_DATA,
-        "df 1st": str(df.index[0]),
-        "df last": str(df.index[-1])
+        # "df 1st": str(df.index[0]),
+        # "df last": str(df.index[-1])
     }
---
+++ utils/log_config.py
@@ -0,0 +1,55 @@
+import json
+import logging
+from datetime import datetime
+
+
+# Custom JSON formatter
+class JsonFormatter(logging.Formatter):
+    def format(self, record) -> str:
+        log_record = {
+            "timestamp": datetime.utcnow().isoformat(),
+            "level": record.levelname,
+            "logger": record.name,
+            "module": record.module,
+            "line": record.lineno,
+            "message": record.getMessage()
+        }
+
+        # Add exception info if available
+        if record.exc_info:
+            log_record["exception"] = self.formatException(record.exc_info)
+
+        return json.dumps(log_record)
+
+# Define the logging configuration
+log_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "json": {
+            "()": JsonFormatter
+        }
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "DEBUG",
+            "formatter": "json",
+            "stream": "ext://sys.stdout",
+        },
+        "rotating_file": {  
+            "class": "logging.handlers.RotatingFileHandler",
+            "level": "INFO",
+            "formatter": "json",
+            "filename": "fastapi.log",
+            "maxBytes": 10485760,  # 10 MB
+            "backupCount": 3,
+        },
+    },
+        
+    "loggers": {
+        "app": {"handlers": ["console", "rotating_file"], "level": "DEBUG", "propagate": False},
+    },
+    "root": {"handlers": ["console"], "level": "DEBUG"},
+}
+
