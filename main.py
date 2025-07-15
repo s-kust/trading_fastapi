d7cdca123c0e21b24dbf38c861e9a12331f2f80a
SergioKushchenko <serhii.kushchenko@appvales.com>
2025-07-15 10:59:12 +0200

    Testing yfinance 1

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
+import yfinance as yf
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
 from constants import S3_BUCKET, S3_FOLDER_DAILY_DATA
-from utils.s3 import read_daily_ohlc_from_s3
+
+# from utils.s3 import read_daily_ohlc_from_s3
 
 load_dotenv()
 app = FastAPI()
 @app.get("/")
 async def root() -> dict:
-    df = read_daily_ohlc_from_s3(ticker="A")
-    
+    # df = read_daily_ohlc_from_s3(ticker="A")
+    df = yf.Ticker("MSFT").history(period='max', interval='1d')
     return {"message": "Hello World 11", 
         "S3_BUCKET": S3_BUCKET,
         "S3_FOLDER_DAILY_DATA": S3_FOLDER_DAILY_DATA,
-        "df": str(df.index[-1])
+        "df 1st": str(df.index[0]),
+        "df last": str(df.index[-1])
     }
