3f4e95f8631cb164ab590aa8900764160747a81c
SergioKushchenko <serhii.kushchenko@appvales.com>
2025-07-15 10:44:28 +0200

    Testing env variables 4

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
 from constants import S3_BUCKET, S3_FOLDER_DAILY_DATA
 from utils.s3 import read_daily_ohlc_from_s3
 
+load_dotenv()
 app = FastAPI()
 @app.get("/")
 async def root() -> dict:
