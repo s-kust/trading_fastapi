5bb64855ded7236dce1f66d9e235150fcbc5b4e7
SergioKushchenko <serhii.kushchenko@appvales.com>
2025-07-15 10:40:46 +0200

    Testing env variables 2

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ app = FastAPI()
 @app.get("/")
 async def root() -> dict:
     # df = read_daily_ohlc_from_s3(ticker="A")
-    aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
-    aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-    aws_region = os.getenv("AWS_REGION")
+    # aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
+    # aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+    # aws_region = os.getenv("AWS_REGION")
     return {"message": "Hello World 10", 
         "S3_BUCKET": S3_BUCKET,
         "S3_FOLDER_DAILY_DATA": S3_FOLDER_DAILY_DATA,
-        "aws_access_key": aws_access_key,
-        "aws_secret_key": aws_secret_key,
-        "aws_region": aws_region,
+        # "aws_access_key": aws_access_key,
+        # "aws_secret_key": aws_secret_key,
+        # "aws_region": aws_region,
+        "os_environ": os.environ
     }
