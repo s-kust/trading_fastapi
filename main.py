cb7405de200c473bb43267fc37a369f620091afa
SergioKushchenko <serhii.kushchenko@appvales.com>
2025-07-15 18:40:29 +0200

    Added exception logging 5

--- main.py
+++ main.py
@@ -10,26 +10,28 @@ from utils.log_config import log_config
 from utils.s3 import read_daily_ohlc_from_s3
 
 dictConfig(log_config)
-logger = logging.getLogger("app")
+app_logger = logging.getLogger("app")
 load_dotenv()
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root() -> dict:
-    ticker="NFLX"
+    ticker = "NFLX"
     try:
         df = read_daily_ohlc_from_s3(ticker=ticker)
     except Exception as e:
-        logger.error(e, exc_info=True)
+        app_logger.error(e, exc_info=True)
         raise e
     # df = yf.Ticker(ticker=ticker).history(period='max', interval='1d')
     msg = f"This is debug message with {ticker=}"
-    logger.debug(msg)
-    return {"message": "Hello World 11", 
+    app_logger.debug(msg)
+    return {
+        "message": "Hello World 11",
         "S3_BUCKET": S3_BUCKET,
         "S3_FOLDER_DAILY_DATA": S3_FOLDER_DAILY_DATA,
-        "msg": msg
+        "msg": msg,
         # "df 1st": str(df.index[0]),
         # "df last": str(df.index[-1])
     }
