ae8d50eab9870dc6841e720553b58dd5992593b7
SergioKushchenko <serhii.kushchenko@appvales.com>
2025-07-18 10:15:10 +0200

    Try filtering DF by valid RSI

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from utils.import_data import (
     import_yahoo_fin_daily,
 )
 from utils.log_config import log_config
-from utils.s3 import read_daily_ohlc_from_s3, write_df_to_s3_csv
+from utils.s3 import read_daily_ohlc_from_s3, read_df_from_s3_csv, write_df_to_s3_csv
 
 dictConfig(log_config)
 app_logger = logging.getLogger("app")
@@ -24,10 +24,19 @@ app = FastAPI()
 
 @app.get("/")
 async def root() -> dict:
-    ticker = "AMZN"
+    ticker = "NFLX"
+
     # df = add_fresh_ohlc_to_ticker_data(ticker=ticker)
-    df = import_yahoo_fin_daily(ticker=ticker)
-    print(df)
+    # df = import_yahoo_fin_daily(ticker=ticker)
+    # print(df)
+
+    filename = f"{ticker.upper()}.csv"
+    df = read_df_from_s3_csv(filename=filename, folder="daily_OHLC_with_RSI/")
+    if df is not None and not df.empty:
+        last_rsi_valid_index = df["RSI_14"].last_valid_index()
+        start_index = max(0, last_rsi_valid_index - 14)
+        print(f"{last_rsi_valid_index=}")
+        print(f"{start_index=}")
 
     return {
         "message": f"Hello World RSI, {ticker=}",
