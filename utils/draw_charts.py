e98070f21e3db3b33364a5141ee30e2e8f03f86c
SergioKushchenko <serhii.kushchenko@appvales.com>
2025-07-18 18:53:21 +0200

    Added draw_save_candlestick_with_rsi 10

--- utils/draw_charts.py
+++ utils/draw_charts.py
@@ -9,94 +9,7 @@ from constants import RSI_PERIOD
 # see https://stackoverflow.com/questions/79204447/kaleido-runtimeerror
 
 
-def draw_save_candlestick_with_rsi(df: pd.DataFrame) -> None:
-    # df_last_30_days = df.last("30D").copy()
-    df_last = df[df.index >= (df.index.max() - pd.Timedelta(days=90))].copy()
-    fig = make_subplots(
-        rows=2,
-        cols=1,
-        shared_xaxes=True,
-        vertical_spacing=0.05,
-        row_heights=[0.7, 0.3],  # Candlestick chart takes 70% height, indicator 30%
-    )
-
-    # Add Candlestick trace to the first row
-    fig.add_trace(
-        go.Candlestick(
-            x=df_last.index,
-            open=df_last["Open"],
-            high=df_last["High"],
-            low=df_last["Low"],
-            close=df_last["Close"],
-            name="OHLC",
-            increasing_line_color="green",  # Green for increasing candles
-            decreasing_line_color="red",  # Red for decreasing candles
-        ),
-        row=1,
-        col=1,
-    )
-
-    # Add Technical Indicator (SMA_10) trace to the second row
-    fig.add_trace(
-        go.Scatter(
-            x=df_last.index,
-            y=df_last[f"RSI_{RSI_PERIOD}"],
-            mode="lines",
-            name=f"RSI_{RSI_PERIOD}",
-            line=dict(color="blue", width=2),
-        ),
-        row=2,
-        col=1,
-    )
-
-    fig.update_layout(
-        title_text="Candlestick Chart with RSI_14",
-        title_x=0.5,  # Center the title
-        xaxis_rangeslider_visible=False,  # Hide the range slider on the bottom x-axis
-        height=700,  # Set overall chart height
-        template="plotly_white",  # Use a clean white background template
-        hovermode="x unified",  # Show hover info for all traces at a given x-coordinate
-    )
-
-    # Update Y-axis titles
-    fig.update_yaxes(title_text="Price", row=1, col=1)
-    fig.update_yaxes(title_text="RSI_14", row=2, col=1)
-
-    # Remove x-axis labels from the top subplot to avoid redundancy
-    fig.update_xaxes(showticklabels=False, row=1, col=1)
-
-    last_date = str(df.index[-1])
-    last_rsi_value = df[f"RSI_{RSI_PERIOD}"].iloc[-1]
-    # last_rsi_value = round(last_rsi_value, 2)
-    last_rsi_value = int(last_rsi_value)
-    fig.update_xaxes(title_text=f"{last_date=}, {last_rsi_value=}", row=2, col=1)
-
-    # Add rangebreaks to remove weekends (Saturday and Sunday)
-    # 6 corresponds to Saturday, 7 to Sunday.
-    # This applies to both x-axes due to shared_xaxes=True
-    fig.update_xaxes(
-        rangebreaks=[
-            dict(
-                bounds=[6, 1], pattern="day of week"
-            )  # Hide Saturday (6) and Sunday (7, which wraps to 1 for Monday)
-        ],
-        row=1,
-        col=1,
-    )
-    fig.update_xaxes(
-        rangebreaks=[
-            dict(
-                bounds=[6, 1], pattern="day of week"
-            )  # Hide Saturday (6) and Sunday (7, which wraps to 1 for Monday)
-        ],
-        row=2,
-        col=1,
-    )
-
-    fig.write_image("fig1.png")
-
-
-def draw_save_candlestick_with_rsi_v2(df: pd.DataFrame) -> None:
+def draw_save_candlestick_with_rsi(df: pd.DataFrame, ticker: str) -> None:
     # df_last_30_days = df.last("30D").copy()
     df_last = df[df.index >= (df.index.max() - pd.Timedelta(days=90))].copy()
     fig = make_subplots(
@@ -149,7 +62,7 @@ def draw_save_candlestick_with_rsi_v2(df: pd.DataFrame) -> None:
     )
 
     fig.update_layout(
-        title_text="Candlestick Chart with RSI_14",
+        title_text=f"{ticker}: Candlestick Chart with RSI_14",
         title_x=0.5,  # Center the title
         xaxis_rangeslider_visible=False,  # Hide the range slider on the bottom x-axis
         height=800,  # Set overall chart height
@@ -159,7 +72,7 @@ def draw_save_candlestick_with_rsi_v2(df: pd.DataFrame) -> None:
 
     # Update Y-axis titles
     fig.update_yaxes(title_text="Price", row=1, col=1)
-    fig.update_yaxes(title_text="Volume", row=3, col=1)
+    fig.update_yaxes(title_text="Volume", row=2, col=1)
     fig.update_yaxes(title_text="RSI_14", row=3, col=1)
 
     # Remove x-axis labels from the top subplot to avoid redundancy
