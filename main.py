dca5590e894097326663364b122475a6a7a94804
SergioKushchenko <serhii.kushchenko@appvales.com>
2025-07-21 16:24:21 +0200

    Switch to relative URLs 2

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import logging
 import os
-import urllib
-
-# import socket
 from logging.config import dictConfig
 from typing import Any
 
@@ -23,24 +20,13 @@ load_dotenv(".env")
 app = FastAPI()
 
 
-def my_url_for(request: Request, name: str, **path_params: Any) -> str:
-    url = request.url_for(name, **path_params)
-    parsed = list(urllib.parse.urlparse(url))
-    # parsed[0] = 'https'  # Change the scheme to 'https' (Optional)
-    parsed[1] = "my_domain.com"  # Change the domain name
-    return urllib.parse.urlunparse(parsed)
-
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
-templates.env.globals["my_url_for"] = my_url_for
 
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request) -> Any:
-    # current_host = get_ip()
-    # print(f"{current_host=}")
-    # print(f"{type(current_host)=}")
+
     return templates.TemplateResponse(
         name="main.html",
         context={
@@ -62,12 +48,10 @@ async def show_rsi_chart(request: Request, ticker: str) -> Any:
     print(f"{img_path_filename=}")
     if not os.path.exists(img_path_filename):
         update_ohlc_rsi_chart(ticker=ticker)
-    img_path_inside_static_dir = "/images/" + ticker + "_RSI.png"
     return templates.TemplateResponse(
         name="img_rsi.html",
         context={
             "ticker": ticker,
-            "img_path": img_path_inside_static_dir,
             "request": request,
         },
     )
