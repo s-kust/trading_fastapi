26f9a1e6ff7d0c7842bbe1cee132df9aabbee415
SergioKushchenko <serhii.kushchenko@appvales.com>
2025-07-21 15:23:52 +0200

    Added img_rsi.html 5

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from logging.config import dictConfig
 from typing import Any
 
 from dotenv import load_dotenv
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from constants import LOCAL_IMG_DIRECTORY
+from constants import LOCAL_IMG_DIRECTORY, TICKERS_TO_FOLLOW
 from utils.e2e import update_ohlc_rsi_chart
 from utils.logging import log_config
 
@@ -32,6 +32,26 @@ async def root(request: Request) -> Any:
     log_msg = f"Inside root: {ticker=}"
     app_logger.info(log_msg)
 
+    return templates.TemplateResponse(
+        name="img_rsi.html",
+        context={
+            "ticker": ticker,
+            "img_path_filename": img_path_filename,
+            "request": request,
+        },
+    )
+
+
+@app.get("/rsi/{ticker}", response_class=HTMLResponse)
+async def show_rsi_chart(request: Request, ticker: str) -> Any:
+    if ticker not in TICKERS_TO_FOLLOW:
+        raise HTTPException(
+            status_code=422,
+            detail=f"Ticker {ticker.upper()} is not in TICKERS_TO_FOLLOW",
+        )
+    img_path_filename = LOCAL_IMG_DIRECTORY + f"{ticker}_RSI.png"
+    if not os.path.exists(img_path_filename):
+        update_ohlc_rsi_chart(ticker=ticker)
     return templates.TemplateResponse(
         name="img_rsi.html",
         context={
