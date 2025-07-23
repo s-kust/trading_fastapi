b4be6ad5958997e13a8abf46a7fb9b9fe5763818
SergioKushchenko <serhii.kushchenko@appvales.com>
2025-07-23 07:14:54 +0200

    Changed copper ticker

--- utils/e2e/misc.py
+++ utils/e2e/misc.py
@@ -8,5 +8,6 @@ def update_ohlc_rsi_chart(ticker: str) -> None:
     Update OHLC dataframe, RSI column, and RSI chart for ticker.
     """
     df = add_fresh_ohlc_to_ticker_data(ticker=ticker)
+    print(df)
     df = update_close_rsi_for_ticker(ticker=ticker, initial_ohlc_df=df)
     draw_save_candlestick_with_rsi(df=df, ticker=ticker)
